fa4c85fcd30e89de140b9c6be8937f466d29979d
PrinceEla123 <prince2752s@gmail.com>
2024-05-20 17:38:00 +0530

    Phoneinfo class added

---
+++ src/org/col/PhoneInfo.java
@@ -0,0 +1,35 @@
+package org.col;
+
+public class PhoneInfo {
+	private void phonename() {
+		System.out.println("Mi");
+
+	}
+	private void phoneimei() {
+		System.out.println("12345678909876");
+
+	}
+	private void phonecamera() {
+		System.out.println("48MP");
+
+	}
+	private void phonestorage() {
+		System.out.println("64 GB");
+
+	}
+	private void phoneos() {
+		System.out.println("Color os");
+
+	}
+	public static void main(String[] args) {
+		PhoneInfo p=new PhoneInfo();
+		p.phonename();
+		p.phoneimei();
+		p.phonecamera();
+		p.phonestorage();
+		p.phoneos();
+	}
+	
+	
+
+}
